35c01f42f9834d7f3d524aa34aa40f8a0aea5c3c
kenfrancisadams <kenfrancisadams@users.noreply.github.com>
2017-02-08 12:19:36 +0000

    Initial Commit

---
+++ clear_print_spool.bat
@@ -0,0 +1,43 @@
+@echo off
+
+setlocal
+
+:PROMPT
+SET /P AREYOUSURE=Are you sure you want to clear the whole print spool? (Y/N)?
+SET printers_path="%systemroot%\System32\Spool\Printers\*.*"
+SET exit_message=All print queues have been cleared. Press any key to exit :)
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+timeout /t 1 /nobreak > nul
+
+:: Stop the print spooler service
+echo Step 1: Stopping Print Spooler
+echo ----------------------------------
+net stop spooler
+timeout /t 1 /nobreak > nul
+echo Print Spooler stopped
+timeout /t 1 /nobreak > nul
+echo.
+
+:: Erase all files in the folder
+echo Step 2: Erasing queued print files
+echo ----------------------------------
+del /Q /F /S %printers_path%
+timeout /t 1 /nobreak > nul
+echo.
+echo Print files deleted
+timeout /t 1 /nobreak > nul
+echo.
+
+:: Restart the print spooler service
+echo Step 3: Restarting Print Spooler
+echo ----------------------------------
+net start spooler
+timeout /t 1 /nobreak > nul
+echo Print Spooler restarted
+timeout /t 1 /nobreak > nul
+echo.
+
+pause>nul|set/p = %exit_message%
+
+:END
+endlocal
